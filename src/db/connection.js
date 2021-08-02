66dbf063f0c5927ceb87406329f2ca54d998e552
ASIMWELA <augastinesimwela@gamil.com>
2021-08-02 11:39:53 +0200

    changed config opt to reject opt false

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,10 +4,9 @@ require("dotenv").config();
 const connectionString = `postgres://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const connection = new Sequelize(process.env.DATABASE_URL, {
-  ssl: true,
   dialect: "postgres",
   dialectOptions: {
-    ssl: true,
+    ssl: { require: true, rejectUnauthorized: false },
   },
 });
 connection.sync({ force: true });
