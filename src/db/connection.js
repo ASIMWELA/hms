dcdabc76ff1968257b97c8f03a3a50b9d78b3761
ASIMWELA <augastinesimwela@gamil.com>
2021-08-02 11:28:37 +0200

    ssl

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectionString = `postgres://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const connection = new Sequelize(process.env.DATABASE_URL);
+const connection = new Sequelize(process.env.DATABASE_URL, { ssl: true });
 connection.sync({ force: true });
 
 module.exports = connection;
